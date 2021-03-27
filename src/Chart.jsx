59a4347d12e4579f0b49218de070910ca1d98f45
David Pinilla <david.pinilla@climatetrade.com>
2021-03-27 17:28:00 +0100

    add vault chart component

---
+++ src/Chart.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { useEffect, useState } from 'react';
+import {
+	Chart,
+	LineAdvance,
+	Tooltip,Interval,
+} from "bizcharts";
+import Web3 from 'web3'
+import vaultAIB from './vault.json'
+import EthDater  from 'ethereum-block-by-date';
+import dayjs from 'dayjs'
+
+const VaultChart =()=> {
+
+const Chart =()=> {
+}
+export default VaultChart
