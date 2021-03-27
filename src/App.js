d749b3891dabc9eecd5a0f1882dceb7abee5a8de
David Pinilla <david.pinilla@climatetrade.com>
2021-03-27 17:36:09 +0100

    add chart to the main app component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import 'antd/dist/antd.css';
+import Chart from './Chart';
 function App() {
 
   return (    
     <div className="App">
       <header className="App-header">
-        
+        <Chart/>
       </header>
     </div>
   );
